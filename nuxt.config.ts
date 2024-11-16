eed84d16b46302fd22682365af2052f4346d5210
datthew <datvuong@gmx.de>
2024-11-16 02:06:55 +0100

    finally worked. please stay like this

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,33 +2,39 @@ import {defineNuxtConfig} from 'nuxt/config'
 import vuetify, {transformAssetUrls} from 'vite-plugin-vuetify'
 
 export default defineNuxtConfig({
-    ssr: true,
-    app: {
-        baseURL: '/',
-    },
+  // ssr: true,
+  app: {
+      baseURL: '/',
+  },
 
-    nitro: {
-        preset: 'github_pages'
-    },
-    components: true,
-    //...
-    build: {
-        transpile: ['vuetify'],
-    },
-    modules: [
-        (_options, nuxt) => {
-            nuxt.hooks.hook('vite:extendConfig', (config) => {
-                // @ts-expect-error
-                config.plugins.push(vuetify({autoImport: true}))
-            })
-        },
-        //...
-    ],
-    vite: {
-        vue: {
-            template: {
-                transformAssetUrls,
-            },
-        },
-    },
+  nitro: {
+      preset: 'github_pages'
+  },
+
+  components: true,
+
+  //...
+  build: {
+      transpile: ['vuetify'],
+  },
+
+  modules: [
+      (_options, nuxt) => {
+          nuxt.hooks.hook('vite:extendConfig', (config) => {
+              // @ts-expect-error
+              config.plugins.push(vuetify({autoImport: true}))
+          })
+      },
+      //...
+  ],
+
+  vite: {
+      vue: {
+          template: {
+              transformAssetUrls,
+          },
+      },
+  },
+
+  compatibilityDate: '2024-11-16',
 })
